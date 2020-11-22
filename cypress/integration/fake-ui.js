81cc966d54543dcd9d8772bd6f8668db328ab8fe
Hanzeyang <hanzeyang0217@gmail.com>
2020-11-22 12:47:16 +0900

    添加switch组件 完善路由切换

--- cypress/integration/fake-ui.js
+++ cypress/integration/fake-ui.js
@@ -7,6 +7,12 @@ describe('The Home Page', () => {
     //大屏幕的时候 有菜单 有列表
     cy.contains("菜单1")
     cy.contains("组件列表")
+    //点边栏不消失出内容
+    cy.contains("Switch 组件").click()
+
+    cy.contains("Button 组件")
+    cy.contains("Switch的文档")
+
 
     //小屏幕的时候 没有菜单 没列表 有汉堡包
     cy.visit('#/doc')
@@ -14,5 +20,9 @@ describe('The Home Page', () => {
     cy.reload()
     cy.contains('汉堡').click()
     cy.contains("组件列表")
+    //点边栏 消失 出内容
+    cy.contains("Switch 组件").click()
+    cy.contains("Button 组件").should('not.be.visible')
+    cy.contains("Switch的文档")
   })
 })
