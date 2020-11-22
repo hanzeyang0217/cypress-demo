b85d5161460e9fd4c2741650ff8ad734340295cd
Hanzeyang <hanzeyang0217@gmail.com>
2020-11-22 17:28:17 +0900

    fix style

--- cypress/integration/fake-ui.js
+++ cypress/integration/fake-ui.js
@@ -7,22 +7,27 @@ describe('The Home Page', () => {
     //大屏幕的时候 有菜单 有列表
     cy.contains("菜单1")
     cy.contains("组件列表")
+    cy.contains("主文档")
+    cy.contains("Switch的文档").should('not.be.visible')
     //点边栏不消失出内容
     cy.contains("Switch 组件").click()
-
     cy.contains("Button 组件")
-    cy.contains("Switch的文档")
+    cy.contains("Switch 的文档")
 
 
     //小屏幕的时候 没有菜单 没列表 有汉堡包
     cy.visit('#/doc')
     cy.viewport("iphone-x")
     cy.reload()
+
+    cy.contains("菜单1").should('not.be.visible')
+    cy.contains("组件列表").should('not.be.visible')
+    cy.contains("主文档")
     cy.contains('汉堡').click()
     cy.contains("组件列表")
     //点边栏 消失 出内容
     cy.contains("Switch 组件").click()
-    cy.contains("Button 组件").should('not.be.visible')
-    cy.contains("Switch的文档")
+    cy.contains("Switch 组件").should('not.be.visible')
+    cy.contains("Switch 的文档")
   })
 })
